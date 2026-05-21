778159b05a0f603bced70e0d5996a015fb01cf7a
darquelhanter <darquelhanter1@gmail.com>
2026-05-20 23:12:01 -0300

    fix: forcar novo build no railway

--- vite.config.ts
+++ vite.config.ts
@@ -22,9 +22,6 @@ export default defineConfig({
   server: {
     host: true,
     allowedHosts: ["localhost", "127.0.0.1"],
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
-    },
   },
 });
+
